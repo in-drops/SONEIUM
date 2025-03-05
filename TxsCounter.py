9cf23434b8b5992c73ed05d0a8ede9b0bbbb7c3b
in_drops <indropschannel@gmail.com>
2025-03-06 01:09:21 +0300

    commits 05.03.25

--- TxsCounter.py
+++ TxsCounter.py
@@ -36,7 +36,7 @@ def worker(account: Account) -> None:
 def activity(bot: Bot):
 
     get_user_agent()
-    excel_report = Excel(bot.account, file='MonadActivity.xlsx')
+    excel_report = Excel(bot.account, file='SoneiumActivity.xlsx')
     excel_report.set_cell('Address', f'{bot.account.address}')
     excel_report.set_date('Date')
 
@@ -44,7 +44,7 @@ def activity(bot: Bot):
         onchain_instance = Onchain(bot.account, Chains.SONEIUM)
         nonce = onchain_instance.get_tx_count(address=bot.account.address)
         excel_report.set_cell(f'Txs Count', f'{nonce}')
-        logger.info(f'Данные занесены в таблицу MonadActivity.xls!')
+        logger.info(f'Данные занесены в таблицу SoneiumActivity.xls!')
     except Exception as e:
         logger.error(f'Ошибка в сети {Chains.SONEIUM.name.upper()}: {e}')
 
